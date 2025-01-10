f4a364569864fa4415d6760a60d5ac5d20bf5d35
Samsonidze <119052018+Samsonidze@users.noreply.github.com>
2025-01-11 02:37:38 +0400

    Delete button-content directory

--- button-content/button.js
+++
@@ -1,9 +0,0 @@
-const magicBtnContainer = document.querySelector(".button-container");
-
-magicBtnContainer.addEventListener("click", () => {
-  magicBtnContainer.classList.add("hidden");
-
-  setTimeout(() => {
-    magicBtnContainer.style.display = "none";
-  }, 500);
-});
